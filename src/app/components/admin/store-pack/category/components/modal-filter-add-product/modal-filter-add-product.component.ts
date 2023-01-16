4b79174a97b54ebf925e86adbc8fc2af13cfe6f9
Pedro <pedro.silva.dev.online@gmail.com>
2023-01-16 20:22:53 -0300

    ajustado o modal large com filtro

--- src/app/components/admin/store-pack/category/components/modal-filter-add-product/modal-filter-add-product.component.ts
+++ src/app/components/admin/store-pack/category/components/modal-filter-add-product/modal-filter-add-product.component.ts
@@ -1,15 +1,34 @@
+import { FormBuilder } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-modal-filter-add-product',
-  templateUrl: './modal-filter-add-product.component.html',
-  styleUrls: ['./modal-filter-add-product.component.css']
+	selector: 'app-modal-filter-add-product',
+	templateUrl: './modal-filter-add-product.component.html',
+	styleUrls: ['./modal-filter-add-product.component.css']
 })
 export class ModalFilterAddProductComponent implements OnInit {
 
-  constructor() { }
+	public filterForm!: FormGroup;
 
-  ngOnInit(): void {
-  }
+	constructor(
+    private _formBuilder: FormBuilder
+	) { 
+		//empty
+	}
+
+	ngOnInit(): void {
+		this._createFilterForm();
+	}
+
+
+
+	/***************** METHODS PRIVATE *****************/
+
+	private _createFilterForm(): void {
+		this.filterForm = this._formBuilder.group({
+      
+		});
+	}
 
 }
